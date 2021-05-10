b22ffefbe7bdd64f6c663426de4abc4940778b05
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2021-05-10 11:29:13 +0200

    fixes

--- feature/composumstarter/bin/examplesites-resourceshook.sh
+++ feature/composumstarter/bin/examplesites-resourceshook.sh
@@ -1,27 +1,36 @@
 #!/usr/bin/env bash
 echo copies some composum sites to the package installation directories for automatic installation
-topdir=`dirname $0`
+topdir=$(dirname $0)
+destdir=target/launcher/fileinstall/99
+haveerror=""
+
 source $topdir/../.env
-mkdir -p target/launcher/fileinstall
+mkdir -p $destdir
 
-if test ! -f target/launcher/fileinstall/sitescopiedmarker; then
+function copyArtifact() {
+  mvn -q -B dependency:copy -Dartifact=$1 -DoutputDirectory=$destdir || haveerror=1
+}
 
-  echo "CAUTION: Installing workflow and tenant package, too, for some tests - do not checkin like that, as this doesn't belong to the public stuff."
-  mvn -q -B dependency:copy -Dartifact=com.composum.platform:composum-platform-workflow-package:1.0.1-SNAPSHOT:zip -DoutputDirectory=target/launcher/fileinstall
-  mvn -q -B dependency:copy -Dartifact=com.composum.platform:composum-platform-tenant-package:1.0.1-SNAPSHOT:zip -DoutputDirectory=target/launcher/fileinstall
+if test ! -f $destdir/sitescopiedmarker; then
 
-  mvn -q -B dependency:copy -Dartifact=sites.ist.composum:composum-site-app-package:${COMPOSUM_SITES_VERSION}:zip -DoutputDirectory=target/launcher/fileinstall
-  mvn -q -B dependency:copy -Dartifact=sites.ist.composum:composum-site-content:${COMPOSUM_SITES_VERSION}:zip -DoutputDirectory=target/launcher/fileinstall
+  copyArtifact com.composum.prototype:composum-prototype-assets-pagesintegration-content:${COMPOSUM_SITES_VERSION}:zip
+  copyArtifact com.composum.prototype:composum-prototype-assets-pagesintegration-app-package:${COMPOSUM_SITES_VERSION}:zip
 
-  mvn -q -B dependency:copy -Dartifact=sites.ist.composum:IST-site-test-testpages-app-package:${COMPOSUM_SITES_VERSION}:zip -DoutputDirectory=target/launcher/fileinstall
-  mvn -q -B dependency:copy -Dartifact=sites.ist.composum:IST-site-test-testpages-content:${COMPOSUM_SITES_VERSION}:zip -DoutputDirectory=target/launcher/fileinstall
+  copyArtifact com.composum.prototype:composum-prototype-assets-demo-app-package:${COMPOSUM_SITES_VERSION}:zip
+  copyArtifact com.composum.prototype:composum-prototype-assets-demo-content:${COMPOSUM_SITES_VERSION}:zip
 
-  mvn -q -B dependency:copy -Dartifact=com.composum.prototype:composum-prototype-assets-pagesintegration-content:${COMPOSUM_SITES_VERSION}:zip -DoutputDirectory=target/launcher/fileinstall
-  mvn -q -B dependency:copy -Dartifact=com.composum.prototype:composum-prototype-assets-pagesintegration-app-package:${COMPOSUM_SITES_VERSION}:zip -DoutputDirectory=target/launcher/fileinstall
+  copyArtifact sites.ist.composum:composum-site-app-package:${COMPOSUM_SITES_VERSION}:zip
+  copyArtifact sites.ist.composum:composum-site-content:${COMPOSUM_SITES_VERSION}:zip
 
-  mvn -q -B dependency:copy -Dartifact=com.composum.prototype:composum-prototype-assets-demo-app-package:${COMPOSUM_SITES_VERSION}:zip -DoutputDirectory=target/launcher/fileinstall
-  mvn -q -B dependency:copy -Dartifact=com.composum.prototype:composum-prototype-assets-demo-content:${COMPOSUM_SITES_VERSION}:zip -DoutputDirectory=target/launcher/fileinstall
+  copyArtifact sites.ist.composum:IST-site-test-testpages-app-package:${COMPOSUM_SITES_VERSION}:zip
+  copyArtifact sites.ist.composum:IST-site-test-testpages-content:${COMPOSUM_SITES_VERSION}:zip
 
-  touch target/launcher/fileinstall/sitescopiedmarker
+  # echo "CAUTION: Installing workflow and tenant package, too, for some tests - do not checkin like that, as this doesn't belong to the public stuff."
+  # copyArtifact com.composum.platform:composum-platform-workflow-package:1.0.1-SNAPSHOT:zip
+  # copyArtifact com.composum.platform:composum-platform-tenant-package:1.0.1-SNAPSHOT:zip
+
+  if test -z "$haveerror"; then
+    touch $destdir/sitescopiedmarker
+  fi
 
 fi
