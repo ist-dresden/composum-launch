3393c3a29ad5de354ca961f872a993e7168c0670
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2021-05-12 12:46:18 +0200

    improve travis build

---
+++ .travis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Build script on travis.
+echo "Building for travis branch $TRAVIS_BRANCH"
+set -vx
+mvn -v
+MVN="mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn"
+
+# Build as much as possible to find all errors at once, but
+# do not start deployment until everything was right
+$MVN -fae -P${TRAVIS_BRANCH} clean install &&
+$MVN -P${TRAVIS_BRANCH},deployDocker deploy &&
+echo "Build script done!"
