bb374db65ece7bdb955b85ba85d2b8c830a3a5ec
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2022-03-28 15:13:59 +0200

    move integration test into appropriate packages

--- feature/integrationtest/src/main/java/com/composum/platform/features/integrationtest/StarterReadyRule.java
+++ feature/integrationtest/src/main/java/com/composum/platform/features/integrationtest/StarterReadyRule.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.sling.launchpad;
+package com.composum.platform.features.integrationtest;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
