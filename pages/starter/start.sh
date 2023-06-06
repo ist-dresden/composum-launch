8dd4665ab3726f65afbbc343d29bcc35d1c2a7e9
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2023-06-06 17:36:00 +0200

    put chatgpt into pages/starter

--- pages/starter/start.sh
+++ pages/starter/start.sh
@@ -2,5 +2,6 @@
 echo Start script for quick check
 set -vxe
 cd target
-/bin/rm -fr sling
-exec java -jar composum-launcher-pages-starter-*.jar
+/bin/rm -fr target
+mkdir -p sling/fileinstall
+exec java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=9970 -jar composum-launcher-pages-starter-*.jar -Dsling.fileinstall.dir=fileinstall
